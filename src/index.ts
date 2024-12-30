cda996612b929b58aee9927829533608bb5babba
dardub <darren@offbase.com>
2024-12-29 16:28:58 -0800

    Add basic error logging to web server.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
-const server = Bun.serve({
-  async fetch(req) {
-    const path = new URL(req.url).pathname;
+const LOG_PATH = "./src/logs/bun.log";
 
-    switch (path) {
-        case "/":
+async function main() {
+  try {
+    const server = Bun.serve({
+      async fetch(req) {
+        const path = new URL(req.url).pathname;
+
+        switch (path) {
+          case "/":
             break;
-    
-        default:
+
+          default:
             break;
+        }
+
+        return new Response("Page not found", { status: 404 });
+      },
+      port: 443,
+      tls: {
+        cert: Bun.file("/etc/letsencrypt/live/do-it.darrendub.me/cert.pem"),
+        key: Bun.file("/etc/letsencrypt/live/do-it.darrendub.me/privkey.pem"),
+      },
+    });
+  } catch (error: any) {
+    const errorMessage = `Bun server error occurred\n ${error.message}`;
+    try {
+      const logs = await Bun.file(LOG_PATH).text();
+      await Bun.write(LOG_PATH, logs.concat(errorMessage));
+    } catch (error) {
+      await Bun.write(LOG_PATH, "".concat(errorMessage));
     }
-
-
-    return new Response("Page not found", { status: 404 });
-  },
-  port: 443,
-  tls: {
-    cert: Bun.file("/etc/letsencrypt/live/do-it.darrendub.me/cert.pem"),
-    key: Bun.file("/etc/letsencrypt/live/do-it.darrendub.me/privkey.pem"),
   }
-});
+}
 
-console.log(`Listening on ${server.url}`);
+main();
